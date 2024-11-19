93842e171b7b7d014e827d9de90999527b28bd98
matyasgubis <gubismatyas@gmail.com>
2024-11-19 22:37:04 +0100

    init base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button'
-import {Panel} from 'primeng/panel';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {heroes, Rarity} from '../data/heroes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, Panel],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'aoem_hero_tool';
+  protected readonly heroes = heroes
+  protected readonly Rarity = Rarity;
 }
