96cb82f227e17a6c69d84e467d292456cb5d2665
matyasgubis <gubismatyas@gmail.com>
2024-11-20 22:28:42 +0100

    init dragging, troop addition, troop deletion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {heroes, Rarity} from '../data/heroes';
+import {Hero, heroes, Rarity} from '../data/heroes';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +13,51 @@ export class AppComponent {
   title = 'aoem_hero_tool';
   protected readonly heroes = heroes
   protected readonly Rarity = Rarity;
+  currentlyDragged: EventTarget | null = null;
+  draggedOver: any;
+  draggedHero?: Hero;
+  troops: Troop[] = []
+
+  handleDragstart($event: DragEvent, hero: Hero) {
+    this.currentlyDragged = $event.target;
+    this.draggedHero = hero;
+  }
+
+  handleDragEnd() {
+    this.currentlyDragged = null;
+    if (this.draggedOver) {
+
+    }
+  }
+
+  handleDragOver($event: DragEvent) {
+    if (this.draggedHero) {
+      $event.preventDefault();
+    }
+    this.draggedOver = $event.target;
+  }
+
+  handleDragLeave($event: DragEvent) {
+    if (this.draggedHero) {
+      $event.preventDefault();
+    }
+    this.draggedOver = undefined;
+  }
+
+  handleTroopAddClicked() {
+    this.troops.push({id: this.troops.length + 1});
+  }
+
+  handleRemoveTroop(id: number) {
+    this.troops = this.troops.filter(troop => troop.id !== id);
+    this.troops.forEach((troop, index) => troop.id = index + 1);
+  }
+}
+
+type Troop = {
+  id: number,
+  name?: string,
+  commander?: Hero,
+  secondInAttack?: Hero,
+  thirdInAttack?: Hero
 }
